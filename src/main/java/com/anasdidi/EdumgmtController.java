ea567bd64af2e66e5aff2cebdb808a8aa2da8eb8
Anas Juwaidi <anas.didi95@gmail.com>
2024-07-14 07:50:21 +0000

    ADD: Initial project

---
+++ src/main/java/com/anasdidi/EdumgmtController.java
@@ -0,0 +1,12 @@
+package com.anasdidi;
+
+import io.micronaut.http.annotation.*;
+
+@Controller("/edumgmt")
+public class EdumgmtController {
+
+  @Get(uri = "/", produces = "text/plain")
+  public String index() {
+    return "Example Response";
+  }
+}
