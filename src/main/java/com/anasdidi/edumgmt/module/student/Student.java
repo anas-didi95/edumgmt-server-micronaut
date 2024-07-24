c46611666ad487c41bc99a5152dd0df06f409cf1
Anas Juwaidi <anas.didi95@gmail.com>
2024-07-24 15:23:12 +0000

    ADD: Add database

--- src/main/java/com/anasdidi/edumgmt/module/student/Student.java
+++ src/main/java/com/anasdidi/edumgmt/module/student/Student.java
@@ -1,21 +1,14 @@
 /* (C) 2024 Anas Juwaidi Bin Mohd Jeffry. All rights reserved. */
 package com.anasdidi.edumgmt.module.student;
 
+import io.micronaut.serde.annotation.Serdeable;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Serdeable
 @Entity
-public class Student {
-
-  @Id private Integer id;
-
-  private String name;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-}
+@Table(name = "t_student")
+public record Student(@Id @GeneratedValue(strategy = GenerationType.AUTO) Long id, String name) {}
