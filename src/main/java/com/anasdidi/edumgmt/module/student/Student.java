991e23cca634bc0c1410a9d9c9cd12f09c91ec15
Anas Juwaidi <anas.didi95@gmail.com>
2024-07-28 03:26:27 +0000

    ADD: Add student table script

--- src/main/java/com/anasdidi/edumgmt/module/student/Student.java
+++ src/main/java/com/anasdidi/edumgmt/module/student/Student.java
@@ -2,13 +2,13 @@
 package com.anasdidi.edumgmt.module.student;
 
 import io.micronaut.serde.annotation.Serdeable;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.UUID;
 
 @Serdeable
 @Entity
-@Table(name = "t_student")
-public record Student(@Id @GeneratedValue(strategy = GenerationType.AUTO) Long id, String name) {}
+@Table(name = "T_STUDENT")
+public record Student(@Id @Column(name = "ID") UUID id, @Column(name = "NM") String name) {}
