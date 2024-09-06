de532bf43659863a0be6c04ea22b3dd3c808d340
Anas Juwaidi <anas.didi95@gmail.com>
2024-09-06 01:36:41 +0000

    ADD: Add openapi for auth handler

--- src/main/java/com/anasdidi/edumgmt/Application.java
+++ src/main/java/com/anasdidi/edumgmt/Application.java
@@ -1,6 +1,7 @@
 /* (C) 2024 Anas Juwaidi Bin Mohd Jeffry. All rights reserved. */
 package com.anasdidi.edumgmt;
 
+import io.micronaut.openapi.annotation.OpenAPISecurity;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @OpenAPIDefinition(
     info =
@@ -25,6 +27,7 @@ import io.swagger.v3.oas.annotations.security.SecuritySchemes;
         type = SecuritySchemeType.HTTP,
         scheme = "bearer",
         bearerFormat = "JWT"))
+@OpenAPISecurity(tags = @Tag(name = "auth"))
 public class Application {
 
   public static void main(String[] args) {
